50574fdbb9e04940ea0f9a91a8dec62db1c6e7cd
Alexis Dorado <programador6@sypsolutions.com>
2021-11-10 15:14:22 -0500

    Canalizador para la validación y manejo de excepciones generales

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,16 +1,10 @@
+using Aplication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApi
 {
@@ -26,7 +20,8 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //Matriculamos en WebApi la agrupacion de los servicios en Application.
+            services.AddApplicationLayer();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
