555d35e0ae7fa250a2a48da0edc9290de7a781e1
Alexis Dorado <programador6@sypsolutions.com>
2021-11-10 16:44:08 -0500

    Contecto bd, se agrega frecha creacion y de modificaicon a todas las entidades, se crea una clase para responder de forma comun y organizada a todas las respuestas de la api

---
+++ Aplication/Features/Categorias/Commands/CreateCategoriaCommand/CreateCategoriaCommand.cs
@@ -0,0 +1,22 @@
+﻿using Aplication.Wrappers;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplication.Features.Categorias.Commands.CreateCategoriaCommand
+{
+    public class CreateCategoriaCommand : IRequest<Response<int>>
+    {
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; } = default!;
+    }
+
+    public class CreateCategoriaCommandHandler : IRequestHandler<CreateCategoriaCommand, Response<int>>
+    {
+        public async Task<Response<int>> Handle(CreateCategoriaCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
