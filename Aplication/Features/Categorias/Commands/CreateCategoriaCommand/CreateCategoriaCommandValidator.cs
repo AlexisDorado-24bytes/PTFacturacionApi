acf42d88abc85478d1d607fa8c6db8a6457674ca
Alexis Dorado <programador6@sypsolutions.com>
2021-11-10 20:23:38 -0500

    Validacion de CreateCategoriaCommand con FluentValidation.

---
+++ Aplication/Features/Categorias/Commands/CreateCategoriaCommand/CreateCategoriaCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Aplication.Features.Categorias.Commands.CreateCategoriaCommand
+{
+    internal class CreateCategoriaCommandValidator : AbstractValidator<CreateCategoriaCommand>
+    {
+        public CreateCategoriaCommandValidator()
+        {
+            RuleFor(p => p.Nombre)
+                .NotEmpty().WithMessage("El {PropertyName} no puede estar vacío.")
+                .MaximumLength(80).WithMessage("El {PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+            RuleFor(p => p.Descripcion)
+                .NotEmpty().WithMessage("La {PropertyName} no puede estar vacía.")
+                .MaximumLength(80).WithMessage("La {PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+            RuleFor(p => p.Codigo)
+                .NotEmpty().WithMessage("El {PropertyName} no puede estar vacío.")
+                .MaximumLength(80).WithMessage("El {PropertyName} no debe exceder de {MaxLength} caracteres.");
+
+        }
+    }
+}
