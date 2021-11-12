4e1ae79c5f0035970db8b663bf44c184d1219ceb
Alexis Dorado <programador6@sypsolutions.com>
2021-11-12 03:09:06 -0500

    Creacion de ela entidad Producto y su respectiva migracion

---
+++ Domain/Entities/Factura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class Factura
+    {
+        public Guid FacturaId { get; set; }
+        public DateTime FechaFactura { get; set; }
+        public Guid Codigo { get; set; }
+        public Cliente Cliente { get; set; }
+        public decimal ValorTotalVenta { get; set; }
+
+    }
+}
---
+++ Persistence/Configuration/ProductoConfig.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configuration
+{
+    public class ProductoConfig : IEntityTypeConfiguration<Producto>
+    {
+        public void Configure(EntityTypeBuilder<Producto> builder)
+        {
+            builder.ToTable("Productos");
+            builder.HasKey(p => p.ProductoId);
+            builder.Property(p => p.Nombre).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Descripcion).HasMaxLength(300).IsRequired();
+            builder.Property(p => p.CategoriaProducto).IsRequired();
+            builder.Property(p => p.PrecioCompra).HasColumnType("decimal(18,2)").IsRequired();
+            builder.Property(p => p.PrecioVentaCliente).HasColumnType("decimal(18,2)").IsRequired();
+        }
+
+    }
+}
