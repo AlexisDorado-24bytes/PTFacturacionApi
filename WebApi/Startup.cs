160d8033fc28ea1755afd6d4472e9c28c8923dd2
Alexis Dorado <programador6@sypsolutions.com>
2021-11-16 19:22:52 -0500

    Cambios en startaup

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -42,7 +42,7 @@ namespace WebApi
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Rest Full", Version = "v1", Description = "Api Rest Full para prueba técnica." });
             });
         }
 
